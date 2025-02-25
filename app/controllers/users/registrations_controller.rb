8afb5ca040be088276dd6d1482856632e466b92d
APysnack <60242065+APysnack@users.noreply.github.com>
2025-02-25 12:51:42 -0500

    login logout

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  include RackSessionFix
+  
   respond_to :json
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
@@ -60,4 +62,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  private 
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: { code: 200, message: "Signed up successfully" },
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }
+    else
+      render json:{
+        status: { code: 422, message: "User could not be created. #{resource.errors.full_messages.to_sentence }" }
+      }, status: :unprocessable_entity
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
-    sign_up: 'signup',
-    password: 'forgot',
-    confirmation: 'activate',
-    invitation: 'invite'
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
 
   get "up" => "rails/health#show", as: :rails_health_check
