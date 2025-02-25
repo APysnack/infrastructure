bfe468e70551afc7d23508eaad50ef881551dda6
APysnack <60242065+APysnack@users.noreply.github.com>
2025-02-25 12:17:17 -0500

    jwt

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -306,7 +306,14 @@ Devise.setup do |config|
   config.responder.redirect_status = :see_other
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.secret_key_base
+    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 1.day.to_i
   end
   
 
