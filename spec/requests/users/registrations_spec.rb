aab40a12edbaf5ae1d980357641db6be82d962f2
apysnack <apysnack@gmail.com>
2025-11-18 12:34:55 -0500

    wip

--- spec/requests/users/registrations_spec.rb
+++ spec/requests/users/registrations_spec.rb
@@ -26,23 +26,22 @@ RSpec.describe "Users::Registrations", type: :request do
       it "returns a success JSON message" do
         post "/signup", params: valid_params, as: :json
 
-        expect(response).to have_http_status(:unprocessable_entity)
-        puts json["status"]
+        expect(response).to have_http_status(:ok)
+        json = JSON.parse(response.body)
         expect(json["status"]["code"]).to eq(200)
         expect(json["status"]["message"]).to eq("Signed up successfully")
         expect(json["data"]["email"]).to eq("test@example.com")
       end
     end
 
-    # context "with invalid params" do
-    #   it "returns an error JSON message" do
-    #     post "/users", params: invalid_params, as: :json
+    context "with invalid params" do
+      it "returns an error JSON message" do
+        post "/signup", params: invalid_params, as: :json
 
-    #     expect(response).to have_http_status(:unprocessable_entity)
-    #     json = JSON.parse(response.body)
-    #     expect(json["status"]["code"]).to eq(422)
-    #     expect(json["status"]["message"]).to include("User could not be created")
-    #   end
-    # end
+        expect(response).to have_http_status(422)
+        json = JSON.parse(response.body)
+        expect(json["status"]["message"]).to include("User could not be created")
+      end
+    end
   end
 end
