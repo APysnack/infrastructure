94f6d2a3141d08ded4443bb2d33346446d160919
apysnack <apysnack@gmail.com>
2025-11-21 14:04:55 -0500

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
-  store :settings, accessors: [:theme], coder: JSON
 
   after_create :assign_default_role
-
-  validate :validate_settings, if: -> { settings.present? }
 
   private
 
   def assign_default_role
     self.add_role(:member) if self.roles.blank?
-  end
-
-  def validate_settings
-    return if settings.is_a?(Hash)
-    errors.add(:settings, "must be a valid JSON object")
   end
 end
--- db/migrate/20250221000000_add_settings_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddSettingsToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :settings, :jsonb, default: {}, null: false
-    add_index :users, :settings, using: :gin
-  end
-end
